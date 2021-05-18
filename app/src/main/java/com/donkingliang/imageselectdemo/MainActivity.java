e6b3cbefda01d1f19effda760a1aec1de0c41b73
zhenke.guan <zhenke.guan@hotmail.com>
2021-05-18 10:48:10 +0800

    adapt to new wechat ui

--- app/src/main/java/com/donkingliang/imageselectdemo/MainActivity.java
+++ app/src/main/java/com/donkingliang/imageselectdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -72,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * @param grantResults
      */
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == PERMISSION_WRITE_EXTERNAL_REQUEST_CODE) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
--- imageselector/src/main/java/com/donkingliang/imageselector/adapter/ImagePagerAdapter.java
+++ imageselector/src/main/java/com/donkingliang/imageselector/adapter/ImagePagerAdapter.java
@@ -3,6 +3,7 @@ package com.donkingliang.imageselector.adapter;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
@@ -12,8 +13,7 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.donkingliang.imageselector.entry.Image;
 import com.donkingliang.imageselector.utils.ImageUtil;
@@ -76,12 +76,11 @@ public class ImagePagerAdapter extends PagerAdapter {
         if (image.isGif()) {
             currentView.setScaleType(ImageView.ScaleType.FIT_CENTER);
             Glide.with(mContext).load(isAndroidQ ? image.getUri() : image.getPath()).override(720,1080)
-                    .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
                     .into(currentView);
         } else {
             Glide.with(mContext).asBitmap()
-                    .apply(new RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE))
-                    .load(isAndroidQ ? image.getUri() : image.getPath()).into(new SimpleTarget<Bitmap>(720,1080) {
+                    .load(isAndroidQ ? image.getUri() : image.getPath()).diskCacheStrategy(DiskCacheStrategy.NONE).override(720,1080).into(new CustomTarget<Bitmap>() {
                 @Override
                 public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
                     int bw = resource.getWidth();
@@ -92,6 +91,11 @@ public class ImagePagerAdapter extends PagerAdapter {
                     } else {
                         setBitmap(currentView, resource);
                     }
+                }
+
+                @Override
+                public void onLoadCleared(@Nullable Drawable placeholder) {
+
                 }
             });
         }
